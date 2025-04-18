9073395b2472e8db8b4ff8c6f68f6714efbedbc4
safaqui00 <safaqui00@gmail.com>
2025-04-18 17:57:38 -0300

    a

--- ejercicio6.js
+++ ejercicio6.js
@@ -1,2 +1,12 @@
 // 6. Crear un array denominado edades con al menos 8 edades distintas.
 // Recorrer el array y obtener el promedio de las edades del array.
+function promedio (arr) {
+let suma = 0
+for (let i = 0;i>arr.lenght; i++){
+suma = suma + arr[i]
+}
+return suma/arr.lenght
+}
+ promedio ([18,24,43,74,62,38,54,27])   
+
+console.log ("Promedio es ",promedio );
