92642da7a3da53e9afba0bb78da948dcd0f64c05
MatiasOrtega <mat-1998-ort@hotmail.com>
2025-04-20 23:18:30 -0300

    se movio capitales and actualizarCapital a un modulo separado

--- js/ejercicio20.js
+++ js/ejercicio20.js
@@ -1,31 +1,12 @@
+import { actualizarCapital } from "./ejercicio20-funcion.js";
+
 const paisesSelect = document.getElementById("paises");
 const capitalesSelect = document.getElementById("capitales");
 
-const capitales = {
-    Mexico: "Ciudad de México",
-    EEUU: "Washington",
-    Canada: "Otawa",
-    Argentina: "Buenos Aires",
-    Brasil: "Brasilia",
-    Uruguay: "Montevideo",
-    Chile: "Santiago de Chile",
-    Bolivia: "La Paz"
-    };
+paisesSelect.addEventListener("change", () =>
+  actualizarCapital(paisesSelect, capitalesSelect)
+);
 
-const actualizarCapital = () =>{
-
-        const paisSeleccionado = paisesSelect.value;
-        const capital = capitales[paisSeleccionado];
-    
-        capitalesSelect.innerHTML = "";
-    
-        const opcion = document.createElement("option");
-        opcion.textContent = capital;
-        capitalesSelect.appendChild(opcion);
-    
-        capitalesSelect.disabled = false;
-}
-
-paisesSelect.addEventListener("change", actualizarCapital);
-
-window.addEventListener("load", actualizarCapital);
+window.addEventListener("load", () =>
+  actualizarCapital(paisesSelect, capitalesSelect)
+);
