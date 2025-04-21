ff068f46d79ce2d66e651d58d8ad3cd6bd752eb4
GuanactolayRodrigo <42583662@fi.unju.edu.ar>
2025-04-21 20:41:02 -0300

    Modifico ejercicio (9, 14, 24) cambiando modo de concatenacion.

--- js/ejercicio24Funcion.js
+++ js/ejercicio24Funcion.js
@@ -2,5 +2,5 @@ export const cambiarColor = () => {
     const colors = ['white','blue','green', 'pink', 'red', 'black', 'orange', 'purple', 'aqua', 'silver', 'magenta', 'maroon', 'lime', 'teal', 'navy', 'olive', 'gray', 'fuchsia', 'yellow', 'coral'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
-    alert("El color de fondo cambiará a: " + randomColor);
+    alert(`El color de fondo cambiará a: ${randomColor}`);
 }
