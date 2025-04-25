cc43ceca6dbccbf4ebb8074c5d8727c06012c830
safaqui00 <safaqui00@gmail.com>
2025-04-25 18:31:16 -0300

    mod ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,5 +2,4 @@
 // Mostrar por consola el siguiente texto:
 // Somos (el valor de la variable) y estamos aprendiendo javascript.
 const integrantes = "Matias Gabriel Ortega,Salim Farid Quintar,Lucas Enrique Cari,Rodrigo Alexis Guanactolay y Balcazar Victor Fernando";
-console.log(" Somos " + integrantes + ", y estamos aprendiendo javascript.");
-
+console.log(`Somos ${integrantes} y estamos aprendiendo javascript`)
